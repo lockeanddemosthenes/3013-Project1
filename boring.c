f0d109c8407e2057cc355f937ea7300bdd1f9166
Melanie <mldworak@wpi.edu>
2020-01-24 09:04:36 -0500

    updated documentation; added timer feature

--- boring.c
+++ boring.c
@@ -5,47 +5,86 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <timer.h>
 
 int main(int argc, char *argv[]) {
 	char command[50];
-	int a = 10;
+	int time_elapsed = 10;
 	char *binaryPath = "~";
 	char *arg1 = "ls -al";
 	char *arg2 = "/home";
+	time_t process_start, process_end;
+	int pid_whoami;
+	int pid_last;
+	int pid_ls;
 
+	//---------------------whoami section------------------
+	//start timer
+	process_start = clock();
 	//running whoami
 	printf("running whoami...\n");
 	strcpy(command, "whoami");
 	system(command);
+	//getting pid for retrieving pagefault info
+
+	//end timer
+	process_end = clock();
 
 	//whoami stats
 	printf("-- Statistics--\n");
-	printf("Elapsed time: %d ms\n",a);
-	printf("Page Faults: %d\n",a);
-	printf("Page Faults (reclaimed): %d\n",a);
+	//timer code
+    time_elapsed = process_end - process_start;
+	printf("Elapsed time: %d milliseconds\n", time_elapsed);
+	//pagefaults code
+	printf("Page Faults: %d\n", a);
+	printf("Page Faults (reclaimed): %d\n", a);
+
 	printf("-- End of Statistics--\n\n");
 
-	//running last
+
+	//----------------------last section-----------------------
+	//start timer
+    process_start = clock();
+    //running last
 	printf("running last...\n");
 	strcpy(command, "last");
 	system(command);
+	//getting pid for retrieving pagefault info
+
+	//end timer
+    process_end = clock();
 
 	//last stats
 	printf("-- Statistics--\n");
-	printf("Elapsed time: %d ms\n",a);
-	printf("Page Faults: %d\n",a);
-	printf("Page Faults (reclaimed): %d\n",a);
+	//timer code
+    time_elapsed = process_end - process_start;
+	printf("Elapsed time: %d milliseconds\n", time_elapsed);
+	//pagefaults code
+	printf("Page Faults: %d\n", a);
+	printf("Page Faults (reclaimed): %d\n", a);
 	printf("-- End of Statistics--\n\n");
 
-	//running ls -al /home
+
+	//-------------------ls -al /home section------------------------
+	//start timer
+    process_start = clock();
+    //running ls -al /home
 	printf("running ls...\n");
 	execl(binaryPath, binaryPath, arg1, arg2, NULL);
+    //getting pid for retrieving pagefault info
+
+    //end timer
+    process_end = clock();
+
 
 	//ls -al /home stats
 	printf("-- Statistics--\n");
-	printf("Elapsed time: %d ms\n",a);
-	printf("Page Faults: %d\n",a);
-	printf("Page Faults (reclaimed): %d\n",a);
+	//timer code
+    time_elapsed = process_end - process_start;
+	printf("Elapsed time: %d milliseconds\n", time_elapsed);
+	//pagefaults code
+	printf("Page Faults: %d\n", a);
+	printf("Page Faults (reclaimed): %d\n", a);
 	printf("-- End of Statistics--\n\n");
 	
 
